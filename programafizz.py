8cd23e7c90bcfd4b0b1daca95679e87f3ce6da40
NAHUEL COSENTINO <nahue.cosentino5@gmail.com>
2015-05-28 17:04:50 -0300

    cambio lineas en programafizz.py

--- programafizz.py
+++ programafizz.py
@@ -1,10 +1,10 @@
 for x in xrange(1, 101):
     if x % 15 == 0:
-        print "FizzBuzz"
+        print "multiplo de ambos"
     elif x % 3 == 0:
-        print "Fizz"
+        print "multiplo de 3"
     elif x % 5 == 0:
-        print "Buzz"
+        print "multiplo de 5"
     else:
         print x
 
